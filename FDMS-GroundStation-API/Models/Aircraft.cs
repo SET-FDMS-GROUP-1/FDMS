312ab272ff79cf68765d62955b00612c02d16db9
Nick Aguilar <naguilar9404@conestogac.on.ca>
2025-11-22 18:30:10 -0500

    Add base data models for entity framework and API functionality

---
+++ FDMS-GroundStation-API/Models/Aircraft.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FDMS_GroundStation_API.Models {
+    public class Aircraft {
+        [Key]
+        [StringLength(15)]
+        public string Id { get; set; }
+        public ICollection<GForceData> GForceData { get; set; }
+        public ICollection<AltitudeData> AltitudeData { get; set; }
+    }
+}
