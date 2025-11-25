b06579f04242053b0313b9ca75a93aca423512eb
Alex Simko <alexsimko4@gmail.com>
2025-11-25 16:14:43 -0500

    Docs: header comments for for main app.jsx

--- FDMS-GroundStation-GUI/src/App.jsx
+++ FDMS-GroundStation-GUI/src/App.jsx
@@ -1,8 +1,19 @@
+// NAME          : src/App.jsx
+// PROJECT       : SENG3020 - Flight Data Management System
+// PROGRAMMER    : Alex Simko
+// FIRST VERSION : 2025-11-23
+// DESCRIPTION   : Main application component, providing global context and layout.
+
+
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { GlobalProvider } from './context/GlobalContext.jsx';
 import Header from './components/Header/Header.jsx';
 
+// FUNCTION    : App
+// DESCRIPTION : Main application component, providing global context and layout.
+// PARAMETERS  : None
+// RETURNS     : JSX.Element - App component
 function App() {
   return (
     <GlobalProvider>
