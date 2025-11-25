b440c78837fd8a3a34f90ca0c603aab41ff920b0
Alex Simko <alexsimko4@gmail.com>
2025-11-25 16:06:17 -0500

    Feat: provide a global context for access to shared variables.
    This is done to avoid prop drilling

--- FDMS-GroundStation-GUI/src/App.jsx
+++ FDMS-GroundStation-GUI/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { GlobalProvider } from './context/GlobalContext.jsx';
 
 function App() {
   return (
-    <>
+    <GlobalProvider>
       <h1>FDMS Ground Station GUI. In Development...</h1>
-    </>
+    </GlobalProvider>
   )
 }
 
